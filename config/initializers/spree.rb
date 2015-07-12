c3a0551aea3886588bcb7b00aad8a133fa4406f8
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-07-12 13:58:22 +0545

    removed path

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,18 +1,34 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# Note: If a preference is set here it will be stored within the cache & database upon initialization.
-#       Just removing an entry from this initializer will not make the preference value go away.
-#       Instead you must either set a new value or remove entry, clear cache, and remove database entry.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
-end
+   # Example:
+   # Uncomment to override the default site name.
+  #  config.site_name = "Gazella Running Costumes"
+  #  config.logo = "store/rungazella.png"
 
+  #S3 configuration
+  if Rails.env.production? then
+       #production. Store images on S3.
+       # development will default to local storage
+      attachment_config = {
+      s3_credentials: {
+        access_key_id: ENV["S3_KEY"],
+        secret_access_key: ENV["S3_SECRET"],
+        bucket: ENV["S3_BUCKET"],
+      },
+
+
+      storage:        :s3,
+      s3_headers:     { "Cache-Control" => "max-age=31557600" },
+      s3_protocol:    "https",
+      bucket:         ENV["S3_BUCKET"],
+
+      # path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
+      default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
+      default_style: "product",
+      }
+
+      attachment_config.each do |key, value|
+           Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+      end
+  end
 Spree.user_class = "Spree::User"
+end
